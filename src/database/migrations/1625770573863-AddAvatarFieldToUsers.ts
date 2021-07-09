a79a872ec6b7d5531773944a4cd626652cd9005a
Francisco Silva Santos <fransssantos2011@gmail.com>
2021-07-08 23:40:40 -0300

    upload de imagem concluido

--- src/database/migrations/1625770573863-AddAvatarFieldToUsers.ts
+++ src/database/migrations/1625770573863-AddAvatarFieldToUsers.ts
@@ -1,11 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
 
-export class AddAvatarFieldToUsers1625770573863 implements MigrationInterface {
+export class AddAvatarFieldToUsers1625770573863
+  implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.addColumn('users', new TableColumn({
+        name: 'avatar',
+        type: 'varchar',
+        isNullable: true,
+      }));
     }
 
+
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropColumn('users', 'avatar');
     }
 
 }
